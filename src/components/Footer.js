9d8760b3fa30900412dc0dfb96a52de68754ecb9
Jupath <attila.taligas@gmail.com>
2018-03-15 13:18:15 +0100

    Add repo link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer pb-4 text-center">
-    <h5>View <a href="" className="font-weight-bold">Source on GitHub</a></h5>
+    <h5>View <a href="https://github.com/jupath/React-redux-user-list-project/" target="_blank" className="font-weight-bold">Source on GitHub</a></h5>
   </footer>
 );
 
